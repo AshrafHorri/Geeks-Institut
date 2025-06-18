eb0aa587c6f5551a4cba4dece9652165955d07d8
charyhorri <charyharrry@gmail.com>
2025-06-18 11:48:31 +0100

    exercises_XP

--- Week1/Day1/exercice1.py
+++ Week1/Day1/exercice1.py
@@ -1,4 +1,2 @@
 print("Hello world" * 4)
-result = (99 ** 3) * 8
-print(result)
-my name = "Gemini"
+
---
+++ Week1/Day1/exercice3.py
@@ -0,0 +1,7 @@
+Myname = "achraf"
+user_name = input("what's your name")
+print(user_name)
+if Myname == user_name:
+   print("good")
+else :
+   print("great")
