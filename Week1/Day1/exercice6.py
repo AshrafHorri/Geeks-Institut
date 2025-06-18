4fe1ba0cfb8271af91d3348951c35c7d26c2faae
charyhorri <charyharrry@gmail.com>
2025-06-18 15:14:38 +0100

    exercises XP

---
+++ Week1/Day1/exercice6.py
@@ -0,0 +1,3 @@
+tuple_var = (0, 1, 2, 3, 4)
+tuple_var.add (5, 6)
+print(tuple_var)
---
+++ Week1/Day1/exercice7.py
@@ -0,0 +1,9 @@
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+basket.remove("Banana")
+print(basket)
+basket.remove("Blueberries")
+print(basket)
+basket.append("Kiwi")
+print(basket)
+Apples_count = basket.count("Apples")
+print(f"The number of times 'Apples' appears is: (apple_count)")
