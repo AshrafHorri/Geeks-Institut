394b46b8b85e6239249798159194d8bb71a0973d
charyhorri <charyharrry@gmail.com>
2025-06-23 01:57:49 +0100

    Exercise_XP_GOLD

--- Week1/Day2/Exercise1.py
+++ Week1/Day2/Exercise1.py
@@ -1,5 +1,9 @@
-keys = ['name,' 'Age,' 'City']
-values = ['Achraf,' '28,' 'Casablanca']
-
-my_dictionary = dict(zip(keys, values))
-print(my_dictionary)
+keys = ['name', 'age', 'city']
+values = ["Achraf", 28, "Casablanca"]
+person = dict(zip(keys, values))
+print(person)
+# Output: {'name': 'Achraf', 'age': 28, 'city': 'Casablanca'}
+# The code creates a dictionary by zipping together two lists: keys and values.
+# The keys list contains the names of the fields, and the values list contains the corresponding values.
+# The zip function pairs each key with its correspo,ding value, and the dict function converts these pairs into a dictionary.
+# The resulting dictionary is then printed to the console.
