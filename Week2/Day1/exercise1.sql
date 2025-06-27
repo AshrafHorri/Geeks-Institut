7c95cbd9dd57abfa008f26f7c3de1021fa3e0f0a
AshrafHorri <ashrafhorri@gmail.com>
2025-06-27 01:34:48 +0100

    Exercise XP

--- Week2/Day1/exercise1.sql
+++ Week2/Day1/exercise1.sql
@@ -1,3 +1,9 @@
+CREATE TABLE items (
+    item_id SERIAL PRIMARY KEY,
+    item_name VARCHAR(100) NOT NULL,
+    price INTEGER NOT NULL
+);
+
 CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
